6d2495de9237c510b1d0d24ed91a86ddd8606e36
a.ahmed <a.ahmed@fz-juelich.de>
2022-04-02 16:12:57 +0200

    commit with Product class and category

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,16 @@
 package com.company;
 
+/**
+ * To solve this problem: what is needed:
+ * 1) To print out receipt which contain:
+ * - different products with name, category, price, quantity and tax
+ * 2) Calculation of sales tax according to category
+ * 3) Calculation of price according to category
+ * 4) Calculation of total price
+ *
+ */
+
+
 public class Main {
 
     public static void main(String[] args) {
---
+++ src/com/company/Product.java
@@ -0,0 +1,115 @@
+package com.company;
+
+/**
+ * @author Arif Ahmed
+ * first step to solve this problem:
+ * create a class: with properties: id, name, category, price, quantity and tax
+ */
+
+public class Product {
+    //unique id for each product
+    private int productId;
+    //Product Name: String
+    private String productName;
+    //Product Category: enum
+    private ProductCategory productCategory;
+    //Product price: double
+    private Double productPrice;
+    //Product Quantity: int
+    private int productQuantity;
+    //Product Tax: double
+    private Double productTax;
+
+    //default constructor
+    public Product() {
+    }
+
+    //constructor with all properties
+    public Product(int productId, String productName, ProductCategory productCategory, Double productPrice, int productQuantity, Double productTax) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productCategory = productCategory;
+        this.productPrice = productPrice;
+        this.productQuantity = productQuantity;
+        this.productTax = productTax;
+    }
+
+    //getter and setters:
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public ProductCategory getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(ProductCategory productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public Double getProductTax() {
+        return productTax;
+    }
+
+    public void setProductTax(Double productTax) {
+        this.productTax = productTax;
+    }
+
+    /**
+     * calculation of tax according to: product category (product price X product quantity X tax rate)
+     *  * 1) Basic- no tax
+     *  * 2) Luxury- 10% tax
+     *  * 3) ImportedBasic- 5% tax
+     *  * 4) ImportedLuxury- 15%  tax
+     */
+
+    public void calculateTax(){
+        switch (this.productCategory){
+            case Basic:
+                this.productTax = 0.0;
+                break;
+            case Luxury:
+                this.productTax = this.productPrice * this.productQuantity * 0.10;
+                break;
+            case ImportedBasic:
+                this.productTax = this.productPrice * this.productQuantity * 0.05;
+                break;
+            case ImportedLuxury:
+                this.productTax = this.productPrice * this.productQuantity * 0.15;
+                break;
+            default:
+                System.out.println("Out of category....");
+        }
+    }
+
+
+
+}
